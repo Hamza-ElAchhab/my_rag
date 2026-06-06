a4d8c992d6c2a1cfef9f84cd54542f2a7d2d16fd
hamzall <boute555@gmail.com>
2026-06-06 16:44:22 +0100

    a

--- src/student/__main__.py
+++ src/student/__main__.py
@@ -1,5 +1,16 @@
-import numpy
+from student.classes_types import AnsweredQuestion, MinimalSource
 import uuid
 
 
-print("Hello World")
+lst = [
+    MinimalSource(file_path="/lp/vc", first_character_index=2, last_character_index=25),
+    MinimalSource(file_path="/lp/vsw.py", first_character_index=0, last_character_index=100),
+]
+
+
+try:
+    obj = AnsweredQuestion(question="koko cv?", sources=lst, answer="hmdlh")
+except Exception as e:
+    print(e)
+
+
